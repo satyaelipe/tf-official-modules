4b7f1d5b7f0079b39e2ae49f300da029b1644cc1
Satya Elipe <satya.elipe@gmail.com>
2020-04-14 15:47:23 +0100

    first

---
+++ vpc.tf
@@ -0,0 +1,35 @@
+data "aws_security_group" "default" {
+  name   = "default"
+  vpc_id = module.vpc.vpc_id
+}
+
+module "vpc" {
+  #source = "../../"
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "simple-vpc"
+
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "euw1-az3"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  #enable_ipv6 = true
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  public_subnet_tags = {
+    Name = "overridden-name-public"
+  }
+
+  tags = {
+    Owner       = "selipe"
+    Environment = "dev"
+  }
+
+  vpc_tags = {
+    Name = "simple-vpc"
+  }
+}
